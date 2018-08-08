c234b1680e14fbbf1447a3a41d88d81025194198
zhgromov <eyasnij@gmail.com>
2018-08-08 14:57:54 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
- 	
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+ 	var gulp = require('gulp');
+ 	var sass = require('gulp-sass');
+ 	var browserSync = require('browser-sync');
 
-gulp.task('hello', function() {
-    console.log('Hello Zell');
-  });
 
-gulp.task('sass', function() {
-    gulp.src('./src/sass/*.sass')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('./dist/css/'));
-});
+ 	gulp.task('sass', function() {
+ 	    gulp.src('./src/sass/*.sass')
+ 	        .pipe(sass()) // Using gulp-sass
+ 	        .pipe(gulp.dest('./dist/css/'))
+ 	        .pipe(browserSync.reload({
+ 	            stream: true
+ 	        }))
+ 	})
+
+ 	gulp.task('browserSync', function() {
+ 	    browserSync({
+ 	        server: {
+ 	            baseDir: 'src'
+ 	        },
+ 	    })
+ 	})
+
+
+ 	gulp.task('watch', ['browserSync', 'sass'], function() {
+ 	    gulp.watch('./src/sass/*.sass', ['sass']);
+ 	    // Обновляем браузер при любых изменениях в HTML или JS
+ 	    gulp.watch('./src/*.html', browserSync.reload);
+ 	    gulp.watch('./src/js/**/*.js', browserSync.reload);
+ 	});
